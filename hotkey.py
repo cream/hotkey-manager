27d7ccecd3e0243a714edaab0fca988d889c2e81
Sebastian Billaudelle <stein@cream-project.org>
2010-03-22 02:26:28 +0100

    More complex handling of hotkeys including one HotkeyBroaker object for each client.

--- hotkey.py
+++ hotkey.py
@@ -23,13 +23,15 @@ from Xlib import X
 import gobject
 import gtk.gdk
 import thread
+import weakref
 
 import cream
 import cream.ipc
+from cream.util import random_hash
 
 class HotkeyBinding(gobject.GObject):
     __gsignals__ = {
-        'activate': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_INT, gobject.TYPE_INT))
+        'hotkey-activated': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_INT, gobject.TYPE_INT))
         }
 
     def __init__(self):
@@ -44,19 +46,18 @@ class HotkeyBinding(gobject.GObject):
         self.hotkeys = []
 
 
-    def add_hotkey(self, hotkey):
+    def add_hotkey(self, keyval, modifier_mask):
 
-        keyval, modifier_mask = gtk.accelerator_parse(hotkey)
         if not keyval and not modifiers:
             self.keycode = None
             self.modifier_mask = None
             return
-        
+
         keycode = self.keymap.get_entries_for_keyval(keyval)[0][0]
         modifier_mask = int(modifier_mask)
 
         self.hotkeys.append((keycode, modifier_mask))
-        self.root.grab_key (keycode, modifier_mask, True, X.GrabModeAsync, X.GrabModeSync)
+        self.root.grab_key(keycode, modifier_mask, True, X.GrabModeAsync, X.GrabModeSync)
         self.display.flush()
 
 
@@ -69,7 +70,7 @@ class HotkeyBinding(gobject.GObject):
     def handler(self, keyval, mask):
 
         gtk.gdk.threads_enter()
-        self.emit("activate", keyval, mask)
+        self.emit("hotkey-activated", keyval, mask)
         gtk.gdk.threads_leave()
         return False
 
@@ -79,12 +80,15 @@ class HotkeyBinding(gobject.GObject):
         self.running = True
         wait_for_release = False
         while self.running:
-            event = self.display.next_event()
-            if event.type == X.KeyPress:
-                keyval = self.keymap.get_entries_for_keycode(event.detail)[0][0]
-                modifier_mask = event.state
-                gobject.idle_add(self.handler, keyval, modifier_mask)
-            self.display.allow_events(X.AsyncKeyboard, event.time)
+            try:
+                event = self.display.next_event()
+                if event.type == X.KeyPress:
+                    keyval = self.keymap.get_entries_for_keycode(event.detail)[0][0]
+                    modifier_mask = event.state
+                    gobject.idle_add(self.handler, keyval, modifier_mask)
+                self.display.allow_events(X.AsyncKeyboard, event.time)
+            except:
+                self.display.allow_events(X.AsyncKeyboard, X.CurrentTime)
 
 
     def listen(self):
@@ -99,11 +103,49 @@ class HotkeyBinding(gobject.GObject):
         self.display.close()
 
 
-class HotkeyManager(cream.Module, cream.ipc.Object):
+class HotkeyBroker(cream.ipc.Object):
+
+    __gsignals__ = {
+        'hotkey-added': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_INT, gobject.TYPE_INT)),
+        'hotkey-removed': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (gobject.TYPE_INT, gobject.TYPE_INT)),
+        }
 
     __ipc_signals__ = {
-        'activate': 'ii'
+        'hotkey_activated': ('s', 'org.cream.hotkeys.broker'),
         }
+
+    def __init__(self, manager):
+
+        cream.ipc.Object.__init__(self,
+            'org.cream.hotkeys',
+            '/org/cream/hotkeys/broker_{0}'.format(random_hash(bits=20))
+        )
+
+        self.hotkeys = {}
+        self.hotkeys_by_action = {}
+
+        self.manager = weakref.ref(manager)
+
+
+    def hotkey_activated_cb(self, keyval, modifier_mask):
+
+        if self.hotkeys.has_key((keyval, modifier_mask)):
+            self.emit_signal('hotkey_activated', self.hotkeys[(keyval, modifier_mask)][0])
+
+
+    @cream.ipc.method('ss', 'b', interface='org.cream.hotkeys.broker')
+    def set_hotkey(self, action, hotkey):
+
+        keyval, modifier_mask = gtk.accelerator_parse(hotkey)
+        if self.hotkeys_by_action.has_key(action):
+            del self.hotkeys[self.hotkeys_by_action[action]]
+            del self.hotkeys_by_action[action]
+        self.hotkeys[(keyval, modifier_mask)] = (action, hotkey)
+        self.hotkeys_by_action[action] = (keyval, modifier_mask)
+        self.manager().set_hotkey(keyval, modifier_mask, self)
+
+
+class HotkeyManager(cream.Module, cream.ipc.Object):
 
     def __init__(self):
 
@@ -113,21 +155,31 @@ class HotkeyManager(cream.Module, cream.ipc.Object):
             '/org/cream/hotkeys'
         )
 
+        self.hotkeys = {}
+
         self.binding = HotkeyBinding()
-        self.binding.connect('activate', self.handler)
+        self.binding.connect('hotkey-activated', self.handler)
         self.binding.listen()
 
 
     def handler(self, binding, keyval, modifier_mask):
 
-        self.messages.debug("'{0}' was pressed...".format(gtk.accelerator_get_label(keyval, modifier_mask)))
-        self.emit_signal('activate', keyval, modifier_mask)
+        if self.hotkeys.has_key((keyval, modifier_mask)):
+            broker = self.hotkeys[(keyval, modifier_mask)]
+            broker.hotkey_activated_cb(keyval, modifier_mask)
 
 
-    @cream.ipc.method('s')
-    def register_hotkey(self, hotkey):
+    def set_hotkey(self, keyval, modifier_mask, broker):
 
-        self.binding.add_hotkey(hotkey)
+        self.hotkeys[(keyval, int(modifier_mask))] = broker
+        self.binding.add_hotkey(keyval, int(modifier_mask))
+
+
+    @cream.ipc.method('', 'o')
+    def register(self):
+
+        b = HotkeyBroker(self)
+        return b.__dbus_object_path__
 
 
 if __name__ == '__main__':
