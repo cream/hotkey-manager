eabf4df81b2641502c184e53e03a5349bb22dd76
Sebastian Billaudelle <stein@cream-project.org>
2010-05-30 22:37:13 +0200

    Simplified setup script.

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from cream.dist import setup
 
 setup('src/manifest.xml',
     data_files = [
-        ('share/cream/modules/cream/{pkg_name}', ['src/hotkey.py', 'src/manifest.xml'])
+        ('{module_dir}', ['src/hotkey.py', 'src/manifest.xml'])
         ]
     )
