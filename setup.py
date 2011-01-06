0ad81c3ba5a5e0d8b47a6f3ea6d10d97bb819677
Sebastian Billaudelle <stein@cream-project.org>
2011-01-06 12:14:35 +0100

    Fixed setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,28 @@
-from cream.dist import setup
+from distutils.core import setup
+from distutils.command.install_scripts import install_scripts
 
-setup('src/manifest.xml',
-    data_files = [
-        ('{module_dir}', ['src/hotkey.py', 'src/manifest.xml'])
-        ]
-    )
+class post_install(install_scripts):
+
+    def run(self):
+        install_scripts.run(self)
+
+        from shutil import move
+        for i in self.get_outputs():
+            n = i.replace('.py', '')
+            move(i, n)
+            print "moving '{0}' to '{1}'".format(i, n)
+
+
+ID = 'org.cream.HotkeyManager'
+data_files = [('share/cream/{0}'.format(ID), ['src/manifest.xml'])]
+
+
+setup(
+    name = 'cream-hotkey-manager',
+    version = '0.0.5',
+    author = 'The Cream Project (http://cream-project.org)',
+    url = 'http://github.com/cream/hotkey-manager',
+    data_files = data_files,
+    cmdclass={'install_scripts': post_install},
+    scripts = ['src/hotkey-manager.py']
+)
